fa8133de6f1fb1a597c703c2ca601706cf1b723b
KC Flynn <38958974+flynnkc@users.noreply.github.com>
2025-06-13 15:54:18 -0400

    Minor change to logging filenames

--- deploy/scripts/hourly.sh
+++ deploy/scripts/hourly.sh
@@ -3,7 +3,7 @@
 # Variables
 EXT_DIR=/usr/local/ociextirpater
 VENV=$EXT_DIR/.venv
-CURRENT_DT=$(date +%Y-%m-%dT%H:%M:%S)
+CURRENT_DT=$(date +%Y-%m-%dT%H-%M-%S)
 
 $VENV/bin/python $EXT_DIR/ociextirpate.py -ip -force -c $1 \
 -o compartments -log $2/$CURRENT_DT.log
